2c2de8d6f86cdd20ac4e9a4a4b3734818e696b90
DIVYA SAMTANI <Divyasamtani@gmail.com>
2016-10-06 08:46:42 +0800

    Added Form and Modal

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
+
+
+  // NAV BAR
+
   $('.sidebarbutton').on('click', function() {
     $('.content').toggleClass('isOpen');
   });
+
+  // LIST CREATOR
+
+
 
 
 });
